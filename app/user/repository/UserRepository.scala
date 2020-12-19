dd73776ef33b0dbe7f1f65feb63a42f88007b50e
Matthew Lau <matthewlau@192.168.1.199>
2020-12-19 23:54:22 +0000

    Refactored to use DAO, and Traite

--- app/user/repository/UserRepository.scala
+++ app/user/repository/UserRepository.scala
@@ -2,54 +2,14 @@ package user.repository
 
 import java.util.UUID
 
-import anorm._
-import javax.inject.Inject
-import play.api.db.Database
 import user.models.User
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.Future
 
-class UserRepository @Inject()(db: Database, databaseExecutionContext: ExecutionContext) {
-
-  val parser: RowParser[User] = Macro.namedParser[User]
-
-  def getUsers(): Future[List[User]]  = {
-    Future {
-      db.withConnection { implicit conn =>
-        SQL"SELECT * FROM users".as(parser.*)
-      }
-    }(databaseExecutionContext)
-  }
-
-  def getUserById(id: UUID): Future[Option[User]]  = {
-    Future {
-      db.withConnection { implicit conn =>
-        SQL"SELECT user_id, name, email FROM users WHERE user_id = ${id}::uuid".as(parser.singleOpt)
-      }
-    }(databaseExecutionContext)
-  }
-
-  def createUser(user: User): Future[Boolean]  = {
-    Future {
-      db.withConnection { implicit conn =>
-        SQL"INSERT INTO users (user_id, name, email) VALUES (${user.user_id}::uuid, ${user.name}, ${user.email})".execute()
-      }
-    }(databaseExecutionContext)
-  }
-
-  def deleteUserById(id: UUID): Future[Int]  = {
-    Future {
-      db.withConnection { implicit conn =>
-        SQL"DELETE FROM users WHERE user_id = ${id}::uuid".executeUpdate()
-      }
-    }(databaseExecutionContext)
-  }
-
-  def updateUserById(id: UUID, user: User): Future[Int]  = {
-    Future {
-      db.withConnection { implicit conn =>
-        SQL"UPDATE users SET name = ${user.name}, email = ${user.email} WHERE user_id = ${id}::uuid".executeUpdate()
-      }
-    }(databaseExecutionContext)
-  }
+trait UserRepository {
+  def getUsers(): Future[List[User]]
+  def getUserById(id: UUID): Future[Option[User]]
+  def createUser(user: User): Future[Boolean]
+  def deleteUserById(id: UUID): Future[Int]
+  def updateUserById(id: UUID, user: User): Future[Int]
 }
--- app/user/services/UserService.scala
+++ app/user/services/UserService.scala
@@ -4,17 +4,19 @@ import java.util.UUID
 import javax.inject.Inject
 import play.api.db.Database
 import user.models.User
-import user.repository.UserRepository
+import user.repository.UserRepositoryImpl
 
 import scala.concurrent.{ExecutionContext, Future}
 
-class UserService @Inject()(db: Database, databaseExecutionContext: ExecutionContext, userRepository: UserRepository)  {
+class UserService @Inject()(db: Database,
+                            databaseExecutionContext: ExecutionContext,
+                            userRepository: UserRepositoryImpl) {
 
-  def getUsers(): Future[List[User]]  = {
+  def getUsers(): Future[List[User]] = {
     userRepository.getUsers()
   }
 
-  def getUserById(id: UUID): Future[Option[User]]  = {
+  def getUserById(id: UUID): Future[Option[User]] = {
     userRepository.getUserById(id)
   }
 
@@ -22,11 +24,11 @@ class UserService @Inject()(db: Database, databaseExecutionContext: ExecutionCon
     userRepository.createUser(user)
   }
 
-  def deleteUserById(id: UUID): Future[Int]  = {
+  def deleteUserById(id: UUID): Future[Int] = {
     userRepository.deleteUserById(id)
   }
 
-  def updateUserById(id: UUID, user: User): Future[Int]  = {
+  def updateUserById(id: UUID, user: User): Future[Int] = {
     userRepository.updateUserById(id, user)
   }
 }
